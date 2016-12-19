a9a163cca0c3cd061aaba79db14717640ba725c9
LIUJIANPING <1532722446@qq.com>
2016-12-19 14:04:33 +0800

    验证码

--- src/com/ht/controller/Customerller.java
+++ src/com/ht/controller/Customerller.java
@@ -1,9 +1,9 @@
 package com.ht.controller;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ht.entity.AES;
 import com.ht.entity.Customer;
 import com.ht.entity.sysuser;
 import com.ht.service.interfaces.CustomerService;
@@ -21,15 +22,14 @@ import com.ht.service.interfaces.UserService;
 @Controller
 @RequestMapping("/customer")
 public class Customerller {
+	@Autowired
+	private SysfunctionService sysfunctionService;
+
+	@Autowired
+	private CustomerService customerService;
 	
 	@Autowired
 	private UserService userService;
-	
-	@Autowired
-	private SysfunctionService sysfunctionService;
-	
-	@Autowired
-	private CustomerService customerService;
 	
 	/**
 	 * 跳转到登录页面
@@ -41,7 +41,7 @@ public class Customerller {
 		return "index2";
 	}
 	
-	@RequestMapping(value="/khlogin",method=RequestMethod.POST)
+	/*@RequestMapping(value="/khlogin",method=RequestMethod.POST)
 	public String login(Customer customer,HttpSession session){
 		Map<String,Object> map = new HashMap<>();
 		map.put("cemail", customer.getCemail());
@@ -59,7 +59,7 @@ public class Customerller {
 		
 		return "redirect:/user/index";
 		
-	}
+	}*/
 	/**
 	 * frame中加载main的部分
 	 * @return
@@ -82,4 +82,26 @@ public class Customerller {
 		
 		return "index2";
 	}
+	@RequestMapping("/khlogin")
+	public String login(Customer customer,String code, HttpServletRequest req,HttpSession session) throws Exception{
+		if(code.equals(session.getAttribute("code"))){
+			customer.setCpassword(AES.getInstance().encrypt(customer.getCpassword()));
+			Map<String, Object> map = new HashMap<>();
+			map.put("cemail", customer.getCemail());
+			map.put("cpassword", customer.getCpassword());
+			try{
+					System.out.println("ssssss");
+					sysuser existsysuser = userService.login(customer.getCemail(),customer.getCpassword());
+					session.setAttribute("user", existsysuser);
+					return "redirect:/user/index";
+			}catch(Exception e){
+				System.out.println("TRY");
+				session.setAttribute("LoginError", "账号或密码有误~");
+				return "index2";
+			}
+		}else{
+			session.setAttribute("LoginError", "验证码有误~");
+			return "index2";
+		}
+	}	
 }
